47c310b677c711937ba3911a910a932f4c78e0fa
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2022-06-17 12:19:33 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new #@messageには、Message.newで生成した、Messageモデルのインスタンス情報を代入
+    @room = Room.find(params[:room_id])  #@roomにはparamsに含まれているroom_idを代入
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only:[:edit, :update]
-  resources :rooms, only:[:new, :create]
+  resources :rooms, only:[:new, :create] do
+    resources :messages, only:[:index, :create]
+    # どのルームで投稿されたメッセージなのかをパスから判断できるようにしたいので、ルーティングのネストを利用。
+  end
+  
 end
