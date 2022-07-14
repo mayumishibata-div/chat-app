fb883f699ff36fc1740131b75ab83f41fe8287f7
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2022-07-14 17:19:02 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,7 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :was_attached
   # validatesのunlessオプションにメソッド名を指定することで、「メソッドの返り値がfalseならばバリデーションによる検証を行う」という条件
 
-  def waz_attached
+  def was_attached
     self.image.attached?
     # 画像があればtrue、なければfalseを返す
   end
